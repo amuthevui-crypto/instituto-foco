cf7650476f2c884bb325de5268bebe2836a9954c
Anibal <amuthevui@gmail.com>
2026-05-22 16:24:19 +0200

    Adiciona validação no formulario login

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,31 @@
 console.log("Sistema Instituto Foco iniciado");
+const formulario = document.querySelector("#loginForm");
+
+const usuario = document.querySelector("#usuario");
+
+const senha = document.querySelector("#senha");
+
+const mensagem = document.querySelector("#mensagem");
+
+formulario.addEventListener("submit", function(evento) {
+
+    evento.preventDefault();
+
+    if(usuario.value === "" || senha.value === "") {
+
+        mensagem.textContent = "Preencha todos os campos";
+
+        mensagem.classList.remove("sucesso");
+
+        mensagem.classList.add("erro");
+
+        return;
+    }
+
+    mensagem.textContent = "Login realizado com sucesso";
+
+    mensagem.classList.remove("erro");
+
+    mensagem.classList.add("sucesso");
+
+});
