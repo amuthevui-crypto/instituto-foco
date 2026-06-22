cd6bc723f3a2b7609724745d0721c848de81676f
Anibal <amuthevui@gmail.com>
2026-06-22 12:53:00 +0200

    Implementa login com seleção de perfis

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
 console.log("Sistema Instituto Foco iniciado");
+
 const formulario = document.querySelector("#loginForm");
 
 const usuario = document.querySelector("#usuario");
@@ -7,11 +8,17 @@ const senha = document.querySelector("#senha");
 
 const mensagem = document.querySelector("#mensagem");
 
+const perfil = document.querySelector("#perfil");
+
 formulario.addEventListener("submit", function(evento) {
 
     evento.preventDefault();
 
-    if(usuario.value === "" || senha.value === "") {
+    if(
+        perfil.value === "" ||
+        usuario.value === "" ||
+        senha.value === ""
+    ) {
 
         mensagem.textContent = "Preencha todos os campos";
 
@@ -20,6 +27,7 @@ formulario.addEventListener("submit", function(evento) {
         mensagem.classList.add("erro");
 
         return;
+
     }
 
     mensagem.textContent = "Login realizado com sucesso";
@@ -30,7 +38,24 @@ formulario.addEventListener("submit", function(evento) {
 
     setTimeout(() => {
 
-        window.location.href = "dashboard.html";
+        if(perfil.value === "admin") {
+
+            window.location.href = "dashboard.html";
+
+        }
+
+        else if(perfil.value === "funcionario") {
+
+            window.location.href = "alunos.html";
+
+        }
+
+        else if(perfil.value === "aluno") {
+
+            alert("Portal do aluno ainda será criado");
+
+        }
 
     }, 1000);
+
 });
